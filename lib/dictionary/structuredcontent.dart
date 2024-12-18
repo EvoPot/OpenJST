1e3046abea8e4f26b3e34ad45be897c69d97a07b
evopot <evolvedpot@gmail.com>
2024-12-18 10:08:27 +0300

    image test

--- lib/dictionary/structuredcontent.dart
+++ lib/dictionary/structuredcontent.dart
@@ -1,3 +1,5 @@
+import 'dart:io';
+
 import 'package:flutter/material.dart';
 import 'package:flutter_widget_from_html/flutter_widget_from_html.dart';
 
@@ -17,9 +19,11 @@ class _StructuredcontentState extends State<Structuredcontent> {
     //cant read the code? me too!
 
     if (widget.structure is String) {
-      return Text(widget.structure);
+      return Text(widget
+          .structure); //return the text itself if what we have is raw text
     } else if (widget.structure is List) {
       return ListView.builder(
+          //if its a list build a list of the same widgets owo
           shrinkWrap: true,
           itemCount: widget.structure.length,
           itemBuilder: (context, index) {
@@ -30,6 +34,15 @@ class _StructuredcontentState extends State<Structuredcontent> {
         return Structuredcontent(
             structure: widget.structure[
                 "content"]); //if the inputted map has a 'type', it is likely a 'structured-content'
+      } else {
+        switch (widget.structure["type"]) {
+          case ("text"):
+            return Text(widget
+                .structure["text"]); //why did this need its seperate thing lol
+
+          case ("image"):
+            return Image(image: AssetImage("assets/images/cat.png"));
+        }
       }
     }
     return Text(widget.structure.toString());
