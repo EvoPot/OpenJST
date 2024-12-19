0db5832b8ee1fbfa19eb9ca3826d81e2a94f254e
evopot <evolvedpot@gmail.com>
2024-12-19 22:18:53 +0300

    var to html

--- lib/dictionary/structuredcontent.dart
+++ lib/dictionary/structuredcontent.dart
@@ -14,16 +14,53 @@ class Structuredcontent extends StatefulWidget {
 }
 
 class _StructuredcontentState extends State<Structuredcontent> {
+  //i have no idea how this code even works anymore but im going with it
   String varToHtml(dynamic input) {
     String output = "";
     String additions = "";
+
     if (input is Map) {
-      return ("""<${input["tag"]} 
-                 style="text-decoration-line: ${input["style"] != null && input["style"]["textDecorationLine"] is List ? (input["style"]["textDecorationLine"] ?? []).join(" ") : (input["style"]?["textDecorationLine"] ?? "none")};
-                 font-size: ${input["style"]?["fontSize"] ?? "none"};
-                 font-weight: ${input["style"]?["fontWeight"] ?? "normal"};">
-            ${varToHtml(input["content"])}
-          </${input["tag"]}>"""); // This is NOT readable
+      String styles =
+          """font-style: ${input["style"]?["fontStyle"] ?? "normal"};
+
+                    font-weight: ${input["style"]?["fontWeight"] ?? "normal"};
+
+                    font-size: ${input["style"]?["fontSize"] ?? "16px"};
+
+                    text-decoration-line: ${input["style"]?["textDecorationLine"] is List ? (input["style"]["textDecorationLine"] as List).join(" ") : input["style"]?["textDecorationLine"] ?? "none"};
+
+                    vertical-align: ${input["style"]?["verticalAlign"] ?? "baseline"};
+
+                    text-align: ${input["style"]?["textAlign"] ?? "left"};""";
+
+      if (["ruby", "rp", "rt"].contains(input["tag"])) {
+        return """<${input["tag"]}>${varToHtml(input["content"]).replaceAll("\n", "<br>")}</${input["tag"]}>"""; // this code is sponsored by black magic
+      }
+
+      if (input["tag"] == "img" || input["type"] == "image") {
+        return """<${input["tag"] ?? "img"} src="asset:assets/images/favicon.png" 
+                alt=${input["description"]}
+                width = ${input["width"]?.toString() ?? ""}${input["sizeUnits"] is String ? input["sizeUnits"] : ""}
+                height = ${input["height"]?.toString() ?? ""}${input["sizeUnits"] is String ? input["sizeUnits"] : ""}
+                ${input["pixelated"] == true ? "image-rendering:pixelated" : ""}>""";
+      } else {
+        String colSpanAttr =
+            input["colSpan"] != null ? 'colspan="${input["colSpan"]}"' : '';
+        String rowSpanAttr =
+            input["rowSpan"] != null ? 'rowspan="${input["rowSpan"]}"' : '';
+
+        return ("""
+            <${input["tag"]}
+            style="$styles"
+            $colSpanAttr $rowSpanAttr>
+              ${varToHtml(input["content"]).replaceAll("\n", "<br>")}</${input["tag"]}>
+
+          """
+            .replaceAll(RegExp(r'\s+'), ' ')
+            .trim());
+
+        // This is NOT readable
+      }
     } else if (input is List) {
       for (var i in input) {
         output += varToHtml(i);
@@ -32,7 +69,6 @@ class _StructuredcontentState extends State<Structuredcontent> {
     } else {
       return input.toString();
     }
-    return "null";
   }
 
   @override
@@ -40,7 +76,7 @@ class _StructuredcontentState extends State<Structuredcontent> {
     //cant read the code? me too!
 
     if (widget.structure is String) {
-      return Text(widget
+      return HtmlWidget(widget
           .structure); //return the text itself if what we have is raw text
     } else if (widget.structure is List) {
       return ListView.builder(
@@ -55,37 +91,10 @@ class _StructuredcontentState extends State<Structuredcontent> {
         return Structuredcontent(
             structure: widget.structure[
                 "content"]); //if the inputted map has a 'type', it is likely a 'structured-content'
+      } else if (widget.structure["type"] == "text") {
+        return HtmlWidget(varToHtml(widget.structure["content"]));
       } else {
-        switch (widget.structure["type"]) {
-          case ("text"):
-            return Text(widget
-                .structure["text"]); //why did this need its seperate thing lol
-
-          case ("image"):
-          case ("img"):
-            //favicon is a placeholder, work on a system to make users add their own files from dictionaries
-            return HtmlWidget("""<img src="asset:assets/images/favicon.png" 
-                alt=${widget.structure["description"]}
-                width = ${widget.structure["width"]}
-                height = ${widget.structure["height"]}
-                ${widget.structure["pixelated"] ? "image-rendering:pixelated" : ""}>""");
-        }
-        switch (widget.structure["tag"]) {
-          case ("img"):
-            return HtmlWidget("""<img src="asset:assets/images/favicon.png" 
-                alt=${widget.structure["description"]}
-                width = ${widget.structure["width"].toString()}${widget.structure["sizeUnits"] is String ? widget.structure["sizeUnits"] : ""}
-                height = ${widget.structure["height"].toString()}${widget.structure["sizeUnits"] is String ? widget.structure["sizeUnits"] : ""}
-                ${widget.structure["pixelated"] == true ? "image-rendering:pixelated" : ""}>""");
-          case ("ul"):
-          case ("ol"):
-          case ("table"):
-          case ("div"):
-          case ("ruby"):
-          case ("a"):
-            return HtmlWidget(varToHtml(widget
-                .structure)); // theres probably a better way to write this but im way too stupid for that
-        }
+        return HtmlWidget(varToHtml(widget.structure));
       }
     }
     return Text(widget.structure.toString());
