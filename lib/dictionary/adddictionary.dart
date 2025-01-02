6709081a5c010b520fd92bbe2040336fe89940b6
evopot <evolvedpot@gmail.com>
2025-01-02 11:25:17 +0300

    add dictionary button

--- lib/dictionary/adddictionary.dart
+++ lib/dictionary/adddictionary.dart
@@ -1,5 +1,7 @@
 import 'dart:io';
 
+import 'package:path_provider/path_provider.dart';
+
 import 'package:flutter/material.dart';
 import 'package:file_picker/file_picker.dart';
 
@@ -7,6 +9,8 @@ class AddDictionaryButton extends StatelessWidget {
   const AddDictionaryButton({super.key});
 
   void getZipFile() async {
+    final tempDir = await getTemporaryDirectory();
+    print(tempDir);
     FilePickerResult? result = await FilePicker.platform
         .pickFiles(type: FileType.custom, allowedExtensions: ['zip']);
 
