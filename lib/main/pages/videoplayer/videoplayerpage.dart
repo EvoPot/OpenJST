084a765762df99cf7a1c96497bbe49ac759a24f4
evopot <evolvedpot@gmail.com>
2025-01-07 11:23:05 +0300

    initialize video player button

--- lib/main/pages/videoplayer/videoplayerpage.dart
+++ lib/main/pages/videoplayer/videoplayerpage.dart
@@ -1,10 +1,21 @@
 import 'package:flutter/material.dart';
+import '../../appstyle/colors.dart';
+import 'player/player.dart';
 
 class Videoplayerpage extends StatelessWidget {
   const Videoplayerpage({super.key});
 
+  static final appColors colors =
+      appColors(); //why do  i have to init this lmao
+
   @override
   Widget build(BuildContext context) {
-    return Text('placeholder!!');
+    return Scaffold(
+      backgroundColor: colors.darkGrey,
+      body: TextButton(
+          onPressed: () => Navigator.push(
+              context, MaterialPageRoute(builder: (context) => Player())),
+          child: Text('go to video player ehehe')),
+    );
   }
 }
